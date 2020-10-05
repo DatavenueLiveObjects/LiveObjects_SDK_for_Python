735c8e026e45229e5bfc96617a641f137321e731
Krzszytof <krzysiek.kr6@wp.pl>
2020-10-05 12:36:49 +0200

    Added usage of regular Python on Linux platform

--- main.py
+++ main.py
@@ -1,36 +1,35 @@
-from uLiveObjectsSDK import *
+import os
+import sys
+try:
+	path = os.environ['_']
+	#print(path)
+	from LiveObjectsSDK import *
+except AttributeError:
+	print("Micropython detected!")
+	if sys.platform == "linux":
+		print("running Python3")
+		import os
+		os.system("python3 main.py")
+		sys.exit()
+	else:
+		from LiveObjectsSDK import *
 
-lo = LiveObjects("PythonMQTT", 1883, "Your api key")
+import time
 
-uptime = 0
+lo = LiveObjects("PythonMQTT", 1883, "<api key>")
+
 def foo(arg=""):
-    lo.outputDebug(INFO,"called function with foo arg ", arg)
+    lo.outputDebug(INFO,"Called function with foo argument ", arg)
     return { "blinked" : "5 times"}
 
+lo.addCommand("foo",foo)
+lo.addParameter("messageRate",5 , INT)
+lo.connect()
 last = time.time()
-
-#esp8266 esp32
-# def do_connect():
-#     import network
-#     sta_if = network.WLAN(network.STA_IF)
-#     if not sta_if.isconnected():
-#         print('connecting to network...')
-#         sta_if.active(True)
-#         sta_if.connect('<SSID>', '<PASS>')
-#         while not sta_if.isconnected():
-#             pass
-#     print('network config:', sta_if.ifconfig())
-
-
-if __name__ == '__main__':
-    #do_connect()
-    lo.addCommand("foo",foo)
-    lo.addParameter("messageRate",5 , INT)
-    lo.connect()
-    uptime = time.time()
-    while True:
-        if(time.time()>=last+lo.getParameter("messageRate")):
-            lo.addToPayload("uptime", int(time.time()-uptime))
-            lo.sendData()
-            lo.loop()
-            last = time.time()
+uptime = time.time()
+while True:
+	if time.time()>=last+lo.getParameter("messageRate"):
+		lo.addToPayload("uptime", int(time.time() - uptime) )
+		lo.sendData()
+		lo.loop()
+		last = time.time()
